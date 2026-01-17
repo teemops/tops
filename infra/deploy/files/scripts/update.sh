d716b0870ce3850abafcc6a40a259a58e31a66ad
Ben Fellows <kiwifellows@gmail.com>
2026-01-17 22:40:33 +1300

    infra wip supervisor configs laravel deploy unfinished!

--- infra/deploy/files/scripts/update.sh
+++ infra/deploy/files/scripts/update.sh
@@ -3,17 +3,17 @@
 #e.g. sh ./update.sh dev
 ENV=$1
 #copy zip file from S3
-aws s3 cp s3://$ENV-tops-deploy/.topsbuild/api.zip /tmp/api.zip
+aws s3 cp s3://$ENV-tops-deploy/.topsbuild/app.zip /tmp/app.zip
 #unzip file
-unzip -o /tmp/api.zip -d /srv/apps/tops
+unzip -o /tmp/app.zip -d /srv/apps/tops
 #copy env file for dev down from S3
-aws s3 cp s3://$ENV-tops-deploy/api/app.env /srv/apps/tops/api/.env
+aws s3 cp s3://$ENV-tops-deploy/app/app.env /srv/apps/tops/app/.env
 #load environment vars from environment and combine with above from S3
 echo "PATH=$PATH" > /etc/environment
-cat /srv/apps/tops/api/.env >> /etc/environment
+cat /srv/apps/tops/app/.env >> /etc/environment
 
 #npm install
-cd /srv/apps/tops/api
+cd /srv/apps/tops/app
 npm install
 
 #migrate database
@@ -22,6 +22,8 @@ npx prisma migrate deploy
 #kill all running node processes
 pkill -f node
 
-supervisorctl restart tops-api
-supervisorctl restart tops-status
-#rm -f /tmp/api.zip
+supervisorctl restart tops-account-q
+supervisorctl restart tops-queue
+supervisorctl restart tops-scan-queue
+supervisorctl restart tops-scan-region-q
+#rm -f /tmp/app.zip
